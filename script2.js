5b1fd50416a69d651acf883eb6eb60143e29d6f5
temiclint <mujeebclint@gmail.com>
2020-06-20 23:58:15 +0100

    just practice and assignment, do not open

---
+++ script2.js
@@ -0,0 +1,105 @@
+// var hash ="#"
+// var num = 1
+// var result 
+// for (var i = 0; i <= 8; i++){
+// var result = hash * num
+// num += 1;
+// }
+// console.log(result)
+
+
+var hash = 1;
+var count = 1;
+var result  = hash * count
+for (var i = 1; i<10; i++){
+//     var hash = 1
+// var count = i;
+var result  = hash * count
+    console.log(result);
+    var count= count+ 1
+    
+}
+
+var hash = 1;
+var count = 1;
+
+for (var i = 1; i<10; i++){
+
+var result  = hash * count
+    console.log(result);
+    var count= count+ 1
+    
+}
+let has= "#"
+console.log(has * has)
+
+
+
+let hash = #;
+let count = #;
+
+for (let i = 0; i<10; i++){
+
+let result  = hash + count
+    console.log(result);
+    let count= count+#
+    
+}
+
+
+for (let counter = 1; counter<= 10; counter ++   ){
+    console.log(counter);
+}
+let hash = 1
+for (let counter = "#"; counter.length<= 10; counter +"#"   ){
+    console.log(counter);
+    counter.length=counter.length + 1
+}
+
+// incrementing # print 
+
+let counter ="#"
+for (let hash = 1; hash <8; hash ++){
+    console.log(counter);
+    counter= counter + "#"
+    console.log("");
+}
+
+// alternating # print
+let counter1 ="#  #  #  #  #";
+let counter2="  #  #  #  #  ";
+for(let count = 0; count < 8; count++){
+    if (count % 2){
+        console.log(counter1)  ;
+}else{
+    console.log(counter2);
+}
+}
+// fizz buzz print for numbers divisible by 5 and 3
+
+for(let count2 = 1; count2<=100; count2++){
+    let fived = count2%5==0;
+    if(count2%3==0){
+        console.log("FIZZ");
+    }else if (count2%5 == 0 && count2%3 !=0){
+        console.log("BUZZ");
+    } else if(count2%5 == 0 && count2%3 == 0){
+        console.log("FIZZ BUZZ");
+    }
+    else{
+        console.log(count2);
+    }
+}
+// prints fizzbuzz when number is divisible by 3 and 5, fizz when number is divisible by 3, and buzz when number is divisible by 5
+for(let count2 = 1; count2<=100; count2++){
+ if (count2 % 5 == 0 && count2 % 3 == 0){
+     console.log("FIZZ BUZZ");
+ }else if (count2 % 3 == 0){
+     console.log("FIZZ");
+ }else if (count2 % 5 == 0){
+     console.log("BUZZ");
+
+ }else{
+     console.log(count2);
+ }
+}
